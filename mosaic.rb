b24fff532e9ab31e2504e761514e65375b3df718
curquiza <clementine.urquizar@gmail.com>
2017-10-10 19:02:36 +0200

    Upd : new files for tests

--- mosaic.rb
+++ mosaic.rb
@@ -1,7 +1,18 @@
 require 'rmagick'
 include Magick
 
-cat = ImageList.new("cachou.jpg")
-cat.display
+#cat = ImageList.new("chat1.jpg")
+#cat.minify!
+#cat.write("rslt.jpg")
+
+img = ImageList.new
+img.read("chat1.jpg")
+img.read("chat2.jpg")
+img.read("chat3.jpg")
+img.read("chat4.jpg")
+#momo = img.mosaic
+#momo.write("rslt.jpg")
+img.write("rslt.jpg")
+
 exit
 
