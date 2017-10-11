84ed4bab32cc6258ece68d03c844f9ead65815cb
curquiza <clementine.urquizar@gmail.com>
2017-10-11 11:17:03 +0200

    Upd : first mosaic created

--- mosaic.rb
+++ mosaic.rb
@@ -10,9 +10,22 @@ img.read("chat1.jpg")
 img.read("chat2.jpg")
 img.read("chat3.jpg")
 img.read("chat4.jpg")
-#momo = img.mosaic
-#momo.write("rslt.jpg")
-img.write("rslt.jpg")
 
-exit
 
+cpy = ImageList.new
+page = Rectangle.new(0, 0, 0, 0)
+4.times do |i|
+	cpy << img.scale(110, 100)
+	page.x = i * cpy.columns
+	page.y = 0
+	cpy.page = page
+	(img.scene += 1) rescue img.scene = 0
+end
+
+momo = cpy.mosaic
+#momo = cpy.montage {
+#	#self.compose = UndefinedCompositeOp
+#	self.frame = "200x200+0+0"
+#	self.border_width = 0
+#}
+momo.write("rslt.jpg")
