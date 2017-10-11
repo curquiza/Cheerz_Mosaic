ba8968eb7c6b152b5f5cf59930fb20a4f3a66648
curquiza <clementine.urquizar@gmail.com>
2017-10-11 12:57:11 +0200

    Upd : back to mosaic, is working with images with different sizes

--- mosaic.rb
+++ mosaic.rb
@@ -5,14 +5,13 @@ include Magick
 #cat.minify!
 #cat.write("rslt.jpg")
 
-rslt = Image.new(1000, 1000)
+rslt = Image.new(2000, 800)
 
 img = ImageList.new
 img.read("chat1.jpg")
 img.read("chat2.jpg")
 img.read("chat3.jpg")
 img.read("chat4.jpg")
-
 
 #cpy = ImageList.new
 #page = Rectangle.new(0, 0, 0, 0)
@@ -24,14 +23,25 @@ img.read("chat4.jpg")
 #	(img.scene += 1) rescue img.scene = 0
 #end
 
-#momo = cpy.mosaic
+col = 0
+row = 0
+page = Rectangle.new(0, 0, 0, 0)
+4.times do
+	page.x = col
+	col += img.columns
+	puts "col = #{col}"
+	puts "page.x = #{page.x}"
+	page.y = 0
+	img.page = page
+	(img.scene += 1) rescue img.scene = 0
+end
 
-momo = img.montage {
-	#self.compose = UndefinedCompositeOp
-	self.tile = "4x1"
-	self.geometry = "200x200+0+0"
-	self.border_width = 0
-}
+momo = img.mosaic
+
+#momo = img.montage {
+#	self.tile = "2x2"
+#	self.geometry = "200x200+0+0"
+#}
 
 rslt.composite!(momo, 0, 0, OverCompositeOp)
 
