f67bd1d79f153dd17033aecb30c42c672f5e2a77
curquiza <clementine.urquizar@gmail.com>
2017-10-11 15:16:17 +0200

    Supp : jpg files removed. Only URL address used

--- mosaic.rb
+++ mosaic.rb
@@ -8,10 +8,10 @@ include Magick
 rslt = Image.new(2000, 2000)
 
 img = ImageList.new
-img.read("chat1.jpg")
-img.read("chat2.jpg")
-img.read("chat3.jpg")
-img.read("chat4.jpg")
+img.read("https://www.consoglobe.com/wp-content/uploads/2016/09/shutterstock_172105073-chaton-litieres-pour-chat.jpg")
+img.read("https://www.wanimo.com/veterinaire/images/articles/chat/question_du_mois-chat.jpg")
+img.read("https://www.royalcanin.fr/wp-content/uploads/chat-male-ou-femelle.jpg")
+img.read("http://www.zoomalia.com/cat_img/cat-2.jpg")
 img.read("http://www.animaniacs.fr/wp-content/uploads/2016/05/chat-noir.jpg")
 img.read("https://www.wanimo.com/veterinaire/images/articles/chat/chaton-diarrhee.jpg")
 
